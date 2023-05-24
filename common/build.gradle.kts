21ec0c900ed953b41043ccedc59dfe2fda788e27
navneetset <navneetset@gmail.com>
2023-05-24 13:42:46 +0200

    New proper mapping + added kyori adventure

---
+++ common/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("dev.architectury.loom")
+    id("architectury-plugin")
+}
+
+
+architectury {
+    common("forge", "fabric")
+}
+
+loom {
+    silentMojangMappingsLicense()
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    // The following line declares the mojmap mappings, you may use other mappings as well
+    mappings("net.fabricmc:yarn:1.19.2+build.4:v2")
+    // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
+    // Do NOT use other classes from fabric loader
+    modImplementation("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
+    implementation("net.kyori:adventure-text-minimessage:${property("minimessage_version")}")
+    implementation("net.kyori:adventure-text-serializer-gson:${property("minimessage_version")}")
+    // Remove the next line if you don't want to depend on the API
+    modApi("dev.architectury:architectury:${property("architectury_version")}") { isTransitive = false }
+    modImplementation("com.cobblemon:mod:1.3.1+1.19.2-SNAPSHOT") { isTransitive = false }
+}
