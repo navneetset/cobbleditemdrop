3d68083e3a48965fd17b1d465c15eb136e926fb9
navneetset <56152154+navneetset@users.noreply.github.com>
2023-05-27 21:09:13 +0200

    Renamed where appropriate.

--- common/src/main/kotlin/tech/sethi/pebbles/cobbleditemdrop/CobbledItemDrop.kt
+++ common/src/main/kotlin/tech/sethi/pebbles/cobbleditemdrop/CobbledItemDrop.kt
@@ -2,17 +2,29 @@ package tech.sethi.pebbles.cobbleditemdrop
 
 import com.cobblemon.mod.common.api.battles.model.actor.BattleActor
 import com.cobblemon.mod.common.api.events.CobblemonEvents
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.DoubleArgumentType
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.context.CommandContext
 import dev.architectury.event.EventResult
+import dev.architectury.event.events.common.CommandRegistrationEvent
 import dev.architectury.event.events.common.EntityEvent
 import dev.architectury.event.events.common.LifecycleEvent
+import jdk.jfr.Timespan.SECONDS
 import net.minecraft.entity.Entity
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import org.apache.logging.log4j.LogManager
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+import kotlin.math.roundToInt
 
 object CobbledItemDrop {
     const val MOD_ID = "cobbleditemdrop"
@@ -26,13 +38,14 @@ object CobbledItemDrop {
     )
 
     private fun getRewardForKill(): Int {
-        return (1..3).random()
+        return ((1..3).random() * dropMultiplier).roundToInt()
     }
 
     private fun getRewardForBattleVictory(pokemonLevel: Int): Int {
         val rewardTier = getRewardTierForLevel(pokemonLevel)
-        return (rewardTier.battleVictoryRewardMin..rewardTier.battleVictoryRewardMax).random()
+        return ((rewardTier.battleVictoryRewardMin..rewardTier.battleVictoryRewardMax).random() * dropMultiplier).roundToInt()
     }
+
 
     private fun getRewardTierForLevel(level: Int): RewardTier {
         return rewardTiers.find { level in it.minLevel..it.maxLevel } ?: rewardTiers.last()
@@ -41,6 +54,10 @@ object CobbledItemDrop {
     @JvmStatic
     fun init() {
         LOGGER.info("Pebble's Cobbled Item Drop Initialized!")
+
+        CommandRegistrationEvent.EVENT.register { dispatcher, dedicated, _ ->
+            registerCommands(dispatcher)
+        }
 
         LifecycleEvent.SERVER_STARTED.register { server ->
             EntityEvent.LIVING_DEATH.register(EntityEvent.LivingDeath { entity: Entity, source: DamageSource ->
@@ -117,6 +134,95 @@ object CobbledItemDrop {
             }
         }
     }
+
+    var dropMultiplier = 1.0
+    private var boostTask: ScheduledFuture<*>? = null
+    private val executor = Executors.newSingleThreadScheduledExecutor()
+
+    fun registerCommands(dispatcher: CommandDispatcher<ServerCommandSource>) {
+        dispatcher.register(
+            CommandManager.literal("pebblesdrop")
+                .requires { it.hasPermissionLevel(2) }
+                .then(
+                    CommandManager.literal("boost")
+                        .then(
+                            CommandManager.argument("multiplier", DoubleArgumentType.doubleArg(0.0))
+                                .then(
+                                    CommandManager.argument("duration", IntegerArgumentType.integer(1))
+                                        .executes { ctx: CommandContext<ServerCommandSource> ->
+                                            setDropMultiplier(
+                                                ctx,
+                                                DoubleArgumentType.getDouble(ctx, "multiplier"),
+                                                IntegerArgumentType.getInteger(ctx, "duration")
+                                            )
+                                        }
+                                )
+                        )
+                )
+                .then(
+                    CommandManager.literal("reset")
+                        .executes { ctx: CommandContext<ServerCommandSource> ->
+                            resetMultiplier(ctx)
+                        }
+                )
+                .then(
+                    CommandManager.literal("check")
+                        .executes { ctx: CommandContext<ServerCommandSource> ->
+                            checkBoost(ctx)
+                        }
+                )
+        )
+    }
+
+    private fun setDropMultiplier(
+        ctx: CommandContext<ServerCommandSource>,
+        multiplier: Double,
+        duration: Int
+    ): Int {
+        dropMultiplier = multiplier
+        boostTask?.cancel(false)  // Cancel the previous task if it exists
+        LOGGER.info("Pebble drop multiplier set to $multiplier for $duration minutes.")
+
+        // Schedule a task to reset the multiplier after the given duration
+        boostTask = executor.schedule(
+            {
+                dropMultiplier = 1.0
+                LOGGER.info("Pebble drop multiplier reset to 1.")
+            },
+            duration.toLong(),
+            TimeUnit.MINUTES
+        )
+
+        ctx.source.sendFeedback(
+            Text.literal("Pebble drop multiplier set to $multiplier for $duration minutes."),
+            false
+        )
+
+        return 1  // Return 1 to indicate the command executed successfully
+    }
+
+    private fun resetMultiplier(ctx: CommandContext<ServerCommandSource>): Int {
+        dropMultiplier = 1.0
+        boostTask?.cancel(false)
+        boostTask = null
+        LOGGER.info("Pebble drop multiplier manually reset to 1.")
+        ctx.source.sendFeedback(Text.literal("Pebble drop multiplier manually reset to 1."), false)
+        return 1
+    }
+
+    private fun checkBoost(ctx: CommandContext<ServerCommandSource>): Int {
+        val remainingTime = boostTask?.getDelay(TimeUnit.SECONDS)?.let { TimeUnit.SECONDS.toMinutes(it + 30) }
+        if (remainingTime != null && remainingTime > 0) {
+            ctx.source.sendFeedback(
+                Text.literal("Active boost: x$dropMultiplier. Remaining time: $remainingTime minutes."),
+                false
+            )
+        } else {
+            ctx.source.sendFeedback(Text.literal("No active boost."), false)
+        }
+        return 1
+    }
+
 
     data class RewardTier(
         val minLevel: Int,
